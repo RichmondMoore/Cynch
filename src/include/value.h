a9fd6bfb2c170be7410a903d29a1cfa27f05d08d
Richmond Moore <richmondwmoore01@gmail.com>
2021-07-06 16:12:33 -0400

    Created and implemented basic functionality

---
+++ src/include/value.h
@@ -0,0 +1,18 @@
+#ifndef CYNCH_VALUE_H
+#define CYNCH_VALUE_H
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+	int capacity;
+	int count;
+	Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* arr);
+void writeValueArray(ValueArray* arr, Value value);
+void freeValueArray(ValueArray* arr);
+
+#endif //CYNCH_VALUE_H
---
+++ src/value.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include "include/memory.h"
+#include "include/value.h"
+
+/* Initializes a ValueArray
+ *
+ *  Params:
+ *      arr:        the ValueArray to initialize
+ */
+void initValueArray(ValueArray* arr) {
+	arr->values = NULL;
+	arr->capacity = 0;
+	arr->count = 0;
+}
+
+/* Writes a value to a ValueArray
+ *
+ *  Params:
+ *      arr:        the ValueArray to write to
+ *      value:      the value to be written
+ *
+ *  Returns:
+ *      int:        the offset value of the next instruction
+ */
+void writeValueArray(ValueArray* arr, Value value) {
+	// Allocates more memory to the array if there is no more room
+	if (arr->capacity < arr->count + 1) {
+		int oldCapacity = arr->capacity;
+		arr->capacity = GROW_CAPACITY(oldCapacity);
+		arr->values = GROW_ARRAY(Value, arr->values, oldCapacity, arr->capacity);
+	}
+
+	// Writes the new value to the array and increments the count
+	arr->values[arr->count] = value;
+	arr->count++;
+}
