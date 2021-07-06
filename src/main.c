7e89a7a802b1a7d54cb0c45b4b858807e56089f8
Richmond Moore <richmondwmoore01@gmail.com>
2021-07-06 16:13:05 -0400

    Updated to test chunk implementation and debugging

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include "include/common.h"
+#include "include/chunk.h"
+#include "include/debug.h"
 
-int main() {
-	printf("Hello, World!\n");
+int main(int argc, const char* argv[]) {
+	Chunk chunk;
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+
+	disassembleChunk(&chunk, "test chunk");
+
+	freeChunk(&chunk);
 	return 0;
 }
