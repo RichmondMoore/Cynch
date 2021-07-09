8d77412c4dcd07e9b5cd8aba72c8536ab940c880
Richmond Moore <richmondwmoore01@gmail.com>
2021-07-08 20:17:33 -0400

    Support for tracking source lines, storing constants, and several more instructions.

--- src/include/chunk.h
+++ src/include/chunk.h
@@ -2,21 +2,33 @@
 #define CYNCH_CHUNK_H
 
 #include "common.h"
+#include "value.h"
 
 // List of instructions
 typedef enum {
+	OP_CONSTANT,
+	OP_CONSTANT_LONG,
+	OP_ADD,
+	OP_SUBTRACT,
+	OP_MULTIPLY,
+	OP_DIVIDE,
+	OP_NEGATE,
 	OP_RETURN,
 } OpCode;
 
 // A sequence of bytcode, stored in a dynamic array
 typedef struct {
-	int count;          // Number of data elements
-	int capacity;       // Max size of the array
-	uint8_t* code;      // Pointer to the array
+	int count;              // Number of data elements
+	int capacity;           // Max size of the array
+	uint8_t* code;          // Pointer to the array
+	int* lines;             // Source lines contained by the chunk
+	ValueArray constants;   // Values contained by the chunk
 } Chunk;
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+void writeConstant(Chunk* chunk, Value value, int line);
 void freeChunk(Chunk* chunk);
 
 #endif //CYNCH_CHUNK_H
