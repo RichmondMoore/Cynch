c8110109057ceddf75f1a4aef2aa615d9865f5be
Richmond Moore <richmondwmoore01@gmail.com>
2021-07-09 18:04:29 -0400

    Implemented checks for keywords and letters

--- src/scanner.c
+++ src/scanner.c
@@ -32,6 +32,17 @@ static bool isDigit(char c) {
 	return c >= '0' && c <= '9';
 }
 
+/* Checks if the given character is a letter or underscore
+ *
+ *  Returns:
+ *      True if the character is a letter or underscore, false otherwise.
+ */
+static bool isAlpha(char c) {
+	return (c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z') ||
+			c == '_';
+}
+
 /* Checks if the scanner is at the end of the file (last token is NULL)
  *
  *  Returns:
@@ -148,6 +159,77 @@ static void skipWhitespace() {
 				return;
 		}
 	}
+}
+
+/* Checks a given number of characters to see if they match a reserved identifier
+ *
+ *  Params:
+ *      start:          the first character of the token
+ *      length:         how many more characters are in the reserved keyword
+ *      rest:           the rest of the letters in the reserved keyword
+ *      type:           the TokenType corresponding to the given information
+ *
+ *  Returns:
+ *      Returns the TokenType matching the given keyword information, or a TOKEN_IDENTIFIER.
+ */
+static TokenType checkKeyword(int start, int length, const char* rest, TokenType type) {
+	if (scanner.current - scanner.start == start + length &&
+	    memcmp(scanner.start + start, rest, length) == 0) {
+		return type;
+	}
+
+	return TOKEN_IDENTIFIER;
+}
+
+/* Determines the TokenType that matches the identifier
+ *
+ *  Returns:
+ *      Returns the TokenType matching the identifier
+ */
+static TokenType identifierType() {
+	switch (scanner.start[0]) {
+		case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+		case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+		case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+		case 'f':
+			if (scanner.current - scanner.start > 1) {
+				switch (scanner.start[1]) {
+					case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+					case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+					case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+				}
+			}
+			break;
+		case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+		case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+		case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+		case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+		case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+		case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+		case 't':
+			if (scanner.current - scanner.start > 1) {
+				switch (scanner.start[1]) {
+					case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+					case 'r': return checkKeyword(2, 2, "UE", TOKEN_TRUE);
+				}
+			}
+			break;
+		case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+		case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+	}
+
+	return TOKEN_IDENTIFIER;
+}
+
+/* Detects an identifier and creates a token of that type
+ *
+ *  Returns:
+ *      A token matching the type of the scanned identifier
+ */
+static Token identifier() {
+	while (isAlpha(peek()) || isDigit(peek())) advance();
+
+	return makeToken(identifierType());
 }
 
 /* Creates a number token
