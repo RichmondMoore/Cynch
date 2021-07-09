5cfedf621190016288f77d1851eea953fb5a4815
Richmond Moore <richmondwmoore01@gmail.com>
2021-07-09 14:13:00 -0400

    Implemented basic compiler support

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "include/common.h"
+#include "include/compiler.h"
 #include "include/debug.h"
 #include "include/memory.h"
 #include "include/vm.h"
@@ -84,8 +85,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-	vm.chunk = chunk;
-	vm.ip = vm.chunk->code;
-	return run();
+InterpretResult interpret(const char* source) {
+	compile(source);
+	return INTERPRET_OK;
 }
