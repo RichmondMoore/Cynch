5c95c25d1531490d7a92382963413a5dfd55b60c
Richmond Moore <richmondwmoore01@gmail.com>
2021-07-09 13:23:51 -0400

    Restored files.

---
+++ src/vm.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+#include "include/common.h"
+#include "include/debug.h"
+#include "include/memory.h"
+#include "include/vm.h"
+
+VM vm;
+
+static void resetStack() {
+	vm.stackCount = 0;
+}
+
+
+void initVM() {
+	vm.stack = NULL;
+	vm.stackCapacity = 0;
+	resetStack();
+}
+
+void freeVM() {
+
+}
+
+void push(Value value) {
+	if (vm.stackCapacity < vm.stackCount + 1) {
+		int oldCapacity = vm.stackCapacity;
+		vm.stackCapacity = GROW_CAPACITY(oldCapacity);
+		vm.stack = GROW_ARRAY(Value, vm.stack, oldCapacity, vm.stackCapacity);
+	}
+
+	vm.stack[vm.stackCount] = value;
+	vm.stackCount++;
+}
+
+Value pop() {
+	vm.stackCount--;
+	return vm.stack[vm.stackCount];
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
+
+	for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+		printf("          ");
+		for (int index = 0; index < vm.stackCount; index++) {
+			printf("[ ");
+			printValue(vm.stack[index]);
+			printf(" ]");
+		}
+		printf("\n");
+		disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+		uint8_t instruction;
+		switch (instruction = READ_BYTE()) {
+			case OP_CONSTANT: {
+				Value constant = READ_CONSTANT();
+				push(constant);
+				break;
+			}
+			case OP_ADD:      BINARY_OP(+); break;
+			case OP_SUBTRACT: BINARY_OP(-); break;
+			case OP_MULTIPLY: BINARY_OP(*); break;
+			case OP_DIVIDE:   BINARY_OP(/); break;
+			case OP_NEGATE:   push(-pop()); break;
+			case OP_RETURN: {
+				printValue(pop());
+				printf("\n");
+				return INTERPRET_OK;
+			}
+		}
+	}
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
+}
+
+InterpretResult interpret(Chunk* chunk) {
+	vm.chunk = chunk;
+	vm.ip = vm.chunk->code;
+	return run();
+}
