b765a7695e06288b7f0f32ae0952e3298ecde361
Richmond Moore <richmondwmoore01@gmail.com>
2021-07-08 20:19:18 -0400

    Updated usage example, including the VM, constants, and arithmetic.

--- src/main.c
+++ src/main.c
@@ -1,14 +1,32 @@
 #include "include/common.h"
 #include "include/chunk.h"
 #include "include/debug.h"
+#include "include/vm.h"
 
 int main(int argc, const char* argv[]) {
+	initVM();
+
 	Chunk chunk;
 	initChunk(&chunk);
-	writeChunk(&chunk, OP_RETURN);
+
+	// The writeConstant() function handles both writing the OP_CONSTANT instruction to the chunk and writing the actual
+	// value to it
+	writeConstant(&chunk, 1.2, 123);
+	writeConstant(&chunk, 3.4, 123);
+
+	writeChunk(&chunk, OP_ADD, 123);
+
+	writeConstant(&chunk, 5.6, 123);
+
+	writeChunk(&chunk, OP_DIVIDE, 123);
+	writeChunk(&chunk, OP_NEGATE, 123);
+
+	writeChunk(&chunk, OP_RETURN, 123);
 
 	disassembleChunk(&chunk, "test chunk");
+	interpret(&chunk);
 
+	freeVM();
 	freeChunk(&chunk);
 	return 0;
 }
